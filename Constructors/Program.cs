f055ea182a707d45cb72df9d133dcf5d418baada
bayramtn <bayram.tn@gmail.com>
2021-02-14 01:01:36 +0300

    4.gunOdev2

---
+++ Constructors/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4.gunOdev2
+
+            Customer customer1 = new Customer { Id = 1, FirstName = "Engin", LastName = "Demirog", City = "Ankara" };
+            Customer customer2 = new Customer(2,"Derin","Demirog","Ankara");
+            Console.WriteLine(customer2.FirstName);
+            
+
+
+        }
+    }
+
+    class Customer
+    {
+        //ozellik barindiran class
+        //metod parametreleri camelcase
+        //default constructor
+        public Customer()
+        {
+
+        }
+        public Customer(int id,string firstName,string lastName,string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+
+    }
+}
