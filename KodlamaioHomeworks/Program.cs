7443f320ec25d99425e514ce0b8d415117254369
bayramtn <bayram.tn@gmail.com>
2021-02-14 02:56:20 +0300

    "Hello World!"

--- KodlamaioHomeworks/Program.cs
+++ KodlamaioHomeworks/Program.cs
@@ -8,20 +8,7 @@ namespace KodlamaioHomeworks
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            List<string> isimler = new List<string>();
-            isimler.Add("ali");
-            Console.WriteLine(isimler.Count);
-            isimler.Add("mehmet");
-            isimler.Add("Veli");
-            Console.WriteLine(isimler.Count);
-
-            foreach (string isim in isimler)
-            {
-                Console.WriteLine(isim);
-                   
-
-            }
-
+           
         }
     }
 }
