8c7fa7711c557d8568b78e6cbada7d2ccea35042
bayramtn <bayram.tn@gmail.com>
2021-02-17 23:06:45 +0300

    5.gunOdev3

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    //5.gunOdev3
+    class Program
+    {
+        //abstract class'in nesnesi olusturulmaz.Govdesi yazilmaz.Referans noktasi alinabilir
+        static void Main(string[] args)
+        {
+            Worker workerA = new Worker();
+            workerA.Add();
+            Worker2 workerB = new Worker2();
+            workerB.Add();
+           
+
+
+            Console.ReadLine();
+
+        }
+    }
+
+    //abstract class MyCLass
+    //{
+    //    public abstract void Add();
+    //    abstract public int MyProperty { get; set; }
+    //}
+
+    interface Person
+    {
+        public void Add();
+
+    }
+
+    abstract class PersonAbstract
+    {
+        public abstract void Add();
+    }
+
+    class Worker:Person
+    {
+        public void Add()
+        {
+            Console.WriteLine("worker Added");
+        }
+    }
+
+    class Worker2 : PersonAbstract
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Worker2 added");
+        }
+    }
+}
